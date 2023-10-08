21dfb10848c9418b7b81d8bc0ac77194abb14bc2
Andana Pratama <dancalneo@gmail.com>
2023-10-09 01:12:52 +0700

    Simple name generator app

---
+++ main.py
@@ -0,0 +1,5 @@
+print("Welcome to the Band Name Generator")
+city = input("Which city did you grow up in?\n")
+pet_name = input("What is your pet or its name?\n")
+
+print("Your band name could be: " + city + " " + pet_name)
